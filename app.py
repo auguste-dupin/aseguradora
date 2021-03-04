9a1f441b1f46577b613d696c7531621c2ff9aca8
Milan Lora <milanlora@gmail.com>
2021-03-04 11:12:25 -0400

    Created new main.html and DASA.png

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+distance = 0.1
+
 @app.route("/")
 def index():
     # Load current count
@@ -20,15 +22,14 @@ def index():
     # Render HTML with count variable
     return render_template("index.html", count=count)
 
-@app.route("/name/<name>")
-def get_book_name(name):
-    return "name : {}".format(name)
+@app.route("/test")
+def show_pag1():
+    global distance
+    return render_template("pag1.html", distance=distance)
 
-@app.route("/details")
-def get_book_details():
-    author=request.args.get('author')
-    published=request.args.get('published')
-    return "Author : {}, Published: {}".format(author,published)
+@app.route("/main")
+def main():
+    return render_template("main.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
